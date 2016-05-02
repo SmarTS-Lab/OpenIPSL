59ca536cca6413626ac858169f74ac445a943a37
Tin Rabuzin <rabuzin@kth.se>
2016-05-02 17:34:57 +0200

    no message

--- runTests.py
+++ runTests.py
@@ -1,1 +1,16 @@
+
+from OMPython import OMCSession
+
+# List of the test examples that will be checked
+
+test_list = ["iPSL.Examples.Machines.PSSE.GENSAL"]
+
+
+omc = OMCSession()
+omc.sendExpression("loadModel(Modelica)")
+
+stopTime = 10
+outputInterval = 0.001
+method = 'Rkfix2'
+
 print "Running some tests"
