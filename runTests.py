1dda391e4fcbd607a1ff0794dab01460d139163c
Tin Rabuzin <rabuzin@kth.se>
2016-05-02 18:23:28 +0200

    no message

--- runTests.py
+++ runTests.py
@@ -1,17 +1,24 @@
-
+import sys
 from OMPython import OMCSession
 
 # List of the test examples that will be checked
 
-test_list = ["iPSL.Examples.Machines.PSSE.GENSAL"]
+test_list = ["OpenIPSL.Examples.Machines.PSSE.GENSAL",
+             "OpenIPSL.Examples.Machines.PSSE.GENSAE",
+             "OpenIPSL.Examples.Machines.PSSE.GENROE",
+             "OpenIPSL.Examples.Machines.PSSE.GENROU"]
 
 
 omc = OMCSession()
 omc.sendExpression("loadModel(Modelica)")
-omc.sendExpression("getClassNames()")
+b = omc.sendExpression('loadFile("/OpenIPSL/OpenIPSL/package.mo")')
+a = omc.sendExpression("getClassNames()")
+
 
 stopTime = 10
 outputInterval = 0.001
 method = 'Rkfix2'
 
-print "Running some tests"
+for test in test_list:
+    c = omc.sendExpression("checkModel(%s)" % (test))
+    print c
