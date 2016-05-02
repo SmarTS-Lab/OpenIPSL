092fa00ff5eeaf626e4175c475bf6ed85a49f2dd
Tin Rabuzin <rabuzin@kth.se>
2016-05-02 18:53:20 +0200

    no message

--- runTests.py
+++ runTests.py
@@ -3,22 +3,33 @@ from OMPython import OMCSession
 
 # List of the test examples that will be checked
 
+omc = OMCSession()
+omc.sendExpression("loadModel(Modelica)")
+omc.sendExpression('loadFile("/OpenIPSL/OpenIPSL/package.mo")')
+omc.sendExpression("getClassNames()")
+'''
 test_list = ["OpenIPSL.Examples.Machines.PSSE.GENSAL",
              "OpenIPSL.Examples.Machines.PSSE.GENSAE",
              "OpenIPSL.Examples.Machines.PSSE.GENROE",
              "OpenIPSL.Examples.Machines.PSSE.GENROU"]
+'''
 
 
-omc = OMCSession()
-omc.sendExpression("loadModel(Modelica)")
-b = omc.sendExpression('loadFile("/OpenIPSL/OpenIPSL/package.mo")')
-a = omc.sendExpression("getClassNames()")
-
-
-stopTime = 10
-outputInterval = 0.001
-method = 'Rkfix2'
+# Get the list of all test cases in class DevelopmentExamples
+test_list = omc.sendExpression('getClassNames(OpenIPSL,recursive=true)')
+nFailed = 0
+nPassed = 0
 
 for test in test_list:
-    c = omc.sendExpression("checkModel(%s)" % (test))
-    print c
+    if omc.sendExpression("isModel(%s)" % (test)):
+        passMsg = omc.sendExpression("checkModel(%s)" % (test))
+        failMsg = omc.sendExpression("getErrorString()")
+        if not failMsg:
+            print passMsg
+            nPassed += 1
+        else:
+            print "Model %s failed to check!" % (test)
+            nFailed += 1
+
+if nPassed:
+    sys.exit(1)
